1649d75396cd9e6fdf6425ac1087532059fd3873
Marcelina Niechciał <marcelinaniech@gmail.comm>
2025-03-30 17:07:33 +0200

    processed data + comments

--- data_parkinson@home/data_processing.py
+++ data_parkinson@home/data_processing.py
@@ -4,20 +4,38 @@ import os
 
 
 def drop_gyroscope(data):
-    #for now also dropping pre or post, arm label and tremor
+    """Dropping unnecessery data
+
+    Args:
+        data (df): time-stamped data for one patient with labels
+
+    Returns:
+        data (df): data without columns to_drop
+    """
+
     to_drop = ["gyroscope_x","gyroscope_y","gyroscope_z","arm_label","pre_or_post","tremor_label"]
     columns = [column for column in list(data) if column in to_drop]
     
     return data.drop(columns=columns)
 
+
 def downsample(data):
+    """Downsampling data to 30HZ
+
+    Args:
+        data (df): time-stamped data for one patient with labels
+
+    Returns:
+        data: modified data with 33HZ
+    """
     return data.iloc[::3,:]
+
 
 def walking_to_binary(data):
     """changes free_living_labels into binary encoding where 1 is gait and 0 is non gait
 
     Args:
-        data (df): data for one patient 
+        data (df): time-stamped data for one patient with labels
 
     Returns:
         data: modified data with bianry-gait column and changed column label
@@ -48,6 +66,5 @@ directory_processed = "data_parkinson@home/processed_data"
 for f in os.listdir(directory_origin):
     file = pd.read_parquet(directory_origin + "/" + f)
     processed = process(file)
-    print(processed.head())
     processed.to_parquet(directory_processed + "/" + f)
 
