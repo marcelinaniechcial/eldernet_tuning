98480b34aee4265b86c2df27df5c847cf85ffdb3
Marcelina Niechciał <marcelinaniech@gmail.comm>
2025-04-01 14:52:52 +0200

    splited data into windows and sub folder + added Eldernet repo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # eldernet_tuning
+
+The purpose of this project is tuning ElderNet model (Brand et al, 2024) for detection of gait without other arm activities. 
+
+
+Brand YE, Kluge F, Palmerini L, Paraschiv-Ionescu A, Becker C, Cereatti A, Maetzler W, Sharrack B, Vereijken B, Yarnall AJ, Rochester L, Del Din S, Muller A, Buchman AS, Hausdorff JM, Perlman O. Automated Gait Detection in Older Adults during Daily-Living using Self-Supervised Learning of Wrist-Worn Accelerometer Data: Development and Validation of ElderNet. Res Sq [Preprint]. 2024 Mar 15:rs.3.rs-4102403. doi: 10.21203/rs.3.rs-4102403/v1. Update in: Sci Rep. 2024 Sep 6;14(1):20854. doi: 10.1038/s41598-024-71491-3. PMID: 38559043; PMCID: PMC10980143.
+
+Brand, Y.E., Kluge, F., Palmerini, L. et al. Self-supervised learning of wrist-worn daily living accelerometer data improves the automated detection of gait in older adults. Sci Rep 14, 20854 (2024). https://doi.org/10.1038/s41598-024-71491-3
--- data_parkinson@home/data_processing.py
+++ data_parkinson@home/data_processing.py
@@ -1,6 +1,13 @@
+from scipy.interpolate import CubicSpline
 import pandas as pd
 import numpy as np
 import os
+
+def pd_recognition(data):
+
+    if "pre_or_post" in data:
+        return True
+    return False
 
 
 def drop_gyroscope(data):
@@ -20,15 +27,24 @@ def drop_gyroscope(data):
 
 
 def downsample(data):
-    """Downsampling data to 30HZ
+    """Downsampling data to 30HZ by interpolation for float values and nearest label for string values (free_living_label)
 
     Args:
         data (df): time-stamped data for one patient with labels
 
     Returns:
-        data: modified data with 33HZ
+        data: modified data with 30HZ
     """
-    return data.iloc[::3,:]
+    accelerometer = ["accelerometer_x", "accelerometer_y", "accelerometer_z"]
+    cs = CubicSpline(data.iloc[:, 0], data[accelerometer])
+
+    sampled_time = np.arange(data["time"].iloc[0], data["time"].iloc[-1], 1/30)
+
+    sampled_data = pd.DataFrame(cs(sampled_time),columns=accelerometer)
+    sampled_data.insert(0, "time", sampled_time)
+    sampled_data = pd.merge_asof(sampled_data, data[["time", "free_living_label"]], on="time", direction="nearest")
+
+    return sampled_data
 
 
 def walking_to_binary(data):
@@ -42,14 +58,45 @@ def walking_to_binary(data):
     """
 
     data['free_living_label'] = np.where(
-        (data['free_living_label'] == "Walking") | 
-        (data['free_living_label'] == "Running") |
-        (data['free_living_label'] == "Walking upstairs") |
-        (data['free_living_label'] == "Walking downstairs") |
-        (data['free_living_label'] == "Turning"), 1, 0)
+        (data['free_living_label'] == "Walking"), 1, 0)
     data.columns = ["time", "accelerometer_x", "accelerometer_y", "accelerometer_z", "gait"]
 
     return data
+
+def make_windows(data):
+    """The function splits accelometer data into 300 samples (10s) windows. 
+    Each window is labeled 1 if tthreshold for gait is passed (acceptance_parameter) and 0 otherwise. 
+    
+
+    Args:
+        data (df): _description_
+
+    Returns:
+        windows: array of size (number_of_windows, 3, 300) 
+        labels: array of size (number_of_windows) . It includes labels indicating gait for each window (1 or 0)
+    """
+    windows = []
+    labels = []
+    acceptance_parameter = 0.9
+    accelerometer = ["accelerometer_x", "accelerometer_y", "accelerometer_z"]
+
+    input = data[accelerometer].values
+    output = data["gait"].values
+
+    for i in range(0,data.shape[0]-300,300):
+
+        windows.append(input[i:i+300,:].T)
+
+        if sum(output[i:i+300])>=300*acceptance_parameter:
+            labels.append(1)
+        else:
+            labels.append(0)
+            
+    windows = np.array(windows)
+    labels = np.array(labels)
+
+
+    return windows,labels
 
 def process(data):
     data = drop_gyroscope(data)
@@ -60,11 +107,25 @@ def process(data):
 
 
 directory_origin = "data_parkinson@home/baseline_data"
-directory_processed = "data_parkinson@home/processed_data"
+
+directory_processed_controls = "data_parkinson@home/processed_data/control"
+directory_processed_pd = "data_parkinson@home/processed_data/pd"
+
+#view example file
+# temp = pd.read_parquet(directory_origin  + "/" + "hbv053_LAS.parquet")arquet")
+# print(temp.head())
+
 
 #loading and processing data
 for f in os.listdir(directory_origin):
+
     file = pd.read_parquet(directory_origin + "/" + f)
+
+    if pd_recognition(file):
+        directory = directory_processed_pd
+    else: 
+        directory = directory_processed_controls
+
     processed = process(file)
-    processed.to_parquet(directory_processed + "/" + f)
+    processed.to_parquet(directory + "/" + f)
 
