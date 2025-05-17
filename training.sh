ff6e09453e63dd77624defb8b5be9e91052fdc32
Marcelina Niechciał <marcelinaniech@gmail.comm>
2025-05-17 14:47:39 +0200

    training path changed

--- training.sh
+++ training.sh
@@ -12,14 +12,4 @@ module load 2023
 module load Anaconda3/2023.07-2
 conda activate /projects/0/einf2658/users/mniechcial/envs/myenv
 
-
-# #Set environment variables
-# export PYTHONPATH=$PYTHONPATH:/projects/einf2658/users/TJ/ssl_gait
-# PD_PATH=/projects/einf2658/users/TJ/ssl_gait/processed_data
-
-# #Copy data to scratch
-# echo "Copying data to scratch..."
-# cp -r $PD_PATH/1.preprocessing $TMPDIR/1.preprocessing
-# echo "Done copying!"
-
 python -u training.py 
