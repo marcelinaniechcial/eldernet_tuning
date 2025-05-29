c7a378a9d1ab78f20191f549a82c37a6cb7c0ab2
Marcelina Niechcial <mniechcial@int5.local.snellius.surf.nl>
2025-05-29 15:08:53 +0200

    modified sbatch job

--- eval.sh
+++ eval.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH -J eldernet_tuning_eval
-#SBATCH -N 1    
+#SBATCH -N 1
 #SBATCH -p gpu_a100
 #SBATCH -t 10:00:00
 #SBATCH --gpus-per-node=1
 #SBATCH -o logs/output_eval.%j.out
 
 #Loading modules
-
-module load 2023
-module load Anaconda3/2023.07-2
-conda activate /projects/0/einf2658/users/mniechcial/envs/myenv
+module purge
+module load 2024
+module load Python/3.10.12-GCCcore-13.2.0
 
 python -u evaluations.py 
